12f44a0b291633cc81df69d0db84dc9d5a34c7b9
Luan Ferreira <85955653+luanferreiradev@users.noreply.github.com>
2024-11-26 16:54:08 +0000

    delete

--- src/config/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class DatabaseConfig {
-    public static final String URL = "jdbc:mariadb://localhost:3306/bank_malvader";
-    public static final String USER = "root";
-    public static final String PASSWORD = "2818";
-}
